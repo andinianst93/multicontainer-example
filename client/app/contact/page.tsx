8390f1498b3ab421ac38585a3eda44a3be268859
andinianst93 <andinianst93@gmail.com>
2023-11-03 00:30:52 +0700

    add routes

---
+++ client/app/contact/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Link from 'next/link'
+export default function Contact() {
+  return (
+    <div className='mx-auto max-w-7xl'>
+        <Link href={'/'}>Go Back Home</Link>
+        <div>
+            <h1>Contact Form</h1>
+        </div>
+    </div>
+  )
+}
--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,8 +1,10 @@
 import React from 'react'
 import Fib from '@/components/Fib'
+import Link from 'next/link'
 export default function Home() {
   return (
     <main className='mx-auto max-w-7xl py-16'>
+      <Link href='/contact'>Contact</Link>
       <section className='text-3xl font-semibold text-center mb-12'>
         <h1>Fib Calculator</h1>
         <div className='border border-blue-700 mt-4 w-[100px] mx-auto'></div>
